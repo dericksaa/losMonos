5c14bd54fb295796aef0564e992bb9aeedd7465e
Derick-VP <desarrollo-jr@valenciaproducciones.com>
2024-01-22 16:18:03 -0500

    version 2 final

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const modalTriggers = document.querySelectorAll(".modal-trigger");
+    const closeButtons = document.querySelectorAll(".close-modal");
+  
+    modalTriggers.forEach(function (trigger) {
+      trigger.addEventListener("click", function () {
+        const modalId = this.getAttribute("data-modal-id");
+        const modal = document.getElementById(modalId);
+        modal.style.display = "block";
+      });
+    });
+  
+    closeButtons.forEach(function (button) {
+      button.addEventListener("click", function () {
+        const modalId = this.getAttribute("data-modal-id");
+        const modal = document.getElementById(modalId);
+        modal.style.display = "none";
+      });
+    });
+  });
